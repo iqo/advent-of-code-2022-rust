22c5df4064c8b404e64582fd73c7e95525bf6001
iqo <niklas.brynolf@gmail.com>
2022-12-04 22:45:15 +0100

    more fixes

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -6,7 +6,7 @@ A Y
 B X
 C Z
 ";
-// #[derive(Eq)]
+#[derive(PartialEq)]
 enum PlayerMove {
     Rock,
     Paper,
@@ -18,16 +18,28 @@ enum MatchOutcome {
     Lose,
 }
 
+impl PartialOrd for PlayerMove {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(if self == other {
+            Ordering::Equal
+        } else if self.greater() == *other {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        })
+    }
+}
+
 impl PlayerMove {
     fn player_move(&self) -> i32 {
         match self {
             PlayerMove::Rock => 1,
             PlayerMove::Paper => 2,
-            PlayerMove::Scissor => 3
+            PlayerMove::Scissor => 3,
         }
     }
     // Greater than self
-    fn greater(&self) -> Self{
+    fn greater(&self) -> Self {
         match self {
             PlayerMove::Rock => Self::Paper,
             PlayerMove::Paper => Self::Scissor,
@@ -35,7 +47,7 @@ impl PlayerMove {
         }
     }
     // Less then self
-    fn less(&self) -> Self{
+    fn less(&self) -> Self {
         match self {
             PlayerMove::Rock => Self::Scissor,
             PlayerMove::Paper => Self::Rock,
@@ -49,9 +61,22 @@ impl FromStr for PlayerMove {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "A" | "X" => Ok(Self::Rock),
-            _ => panic!("failed to match {s}")
+            "B" | "Y" => Ok(Self::Paper),
+            "C" | "Z" => Ok(Self::Scissor),
+            _ => panic!("failed to match {s}"),
         }
     }
+}
+
+fn player_move(our: PlayerMove, their: PlayerMove) -> (i32, i32) {
+    let (o, t) = if our == their {
+        (3, 3)
+    } else if our < their {
+        (0, 6)
+    } else {
+        (6, 0)
+    };
+    (o + our.player_move(), t + their.player_move())
 }
 fn main() {
     /*     let file_input: String = read_input_file();
