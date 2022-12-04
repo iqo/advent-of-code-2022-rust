8cc7e3c6222cf81e99bd7bd0bb6fb4b022bf1e2a
iqo <niklas.brynolf@gmail.com>
2022-12-04 12:48:31 +0100

    read from file

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let file_input:String = read_input_file();
+    println!("{}", file_input);
+}
+
+fn read_input_file() -> String {
+    std::fs::read_to_string("src/input.txt").unwrap()
 }
