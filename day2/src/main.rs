e629cc1ff8ac0915bcaeb24f8c77b98879b1ac21
iqo <niklas.brynolf@gmail.com>
2022-12-04 22:10:43 +0100

    start in playerMove impl

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,6 @@
 use std::cmp::Ordering;
+use std::str::FromStr;
+use std::string::ParseError;
 const TESTDATA: &str = "
 A Y
 B X
@@ -22,6 +24,32 @@ impl PlayerMove {
             PlayerMove::Rock => 1,
             PlayerMove::Paper => 2,
             PlayerMove::Scissor => 3
+        }
+    }
+    // Greater than self
+    fn greater(&self) -> Self{
+        match self {
+            PlayerMove::Rock => Self::Paper,
+            PlayerMove::Paper => Self::Scissor,
+            PlayerMove::Scissor => Self::Rock,
+        }
+    }
+    // Less then self
+    fn less(&self) -> Self{
+        match self {
+            PlayerMove::Rock => Self::Scissor,
+            PlayerMove::Paper => Self::Rock,
+            PlayerMove::Scissor => Self::Paper,
+        }
+    }
+}
+
+impl FromStr for PlayerMove {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Self::Rock),
+            _ => panic!("failed to match {s}")
         }
     }
 }
