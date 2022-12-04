a536031f4c62c3919b1f5fcbb8128abe858a0228
iqo <niklas.brynolf@gmail.com>
2022-12-04 15:20:23 +0100

    count calories

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+
 const TESTDATA: &str = "
 1000
 2000
@@ -15,21 +16,28 @@ const TESTDATA: &str = "
 10000
 ";
 fn main() {
-    // let file_input:String = read_input_file();
+    let file_input:String = read_input_file();
     // let input1 = include_str!("input.txt");
     // println!("{}", file_input);
-    print!("{}", TESTDATA);
-    get_test_data_debug();
+    // print!("{}", TESTDATA);
+    get_test_data_debug(&file_input);
 }
 
 fn read_input_file() -> String {
     std::fs::read_to_string("src/input.txt").unwrap()
 }
 
-fn get_test_data_debug() {
-    for line in TESTDATA.lines() {
+fn get_test_data_debug(input: &str) {
+    let mut elves= Vec::new();
+    let mut sum = 0;
+    for line in input.lines() {
         if line.is_empty() {
-           dbg!(line); 
+            elves.push(sum);
+            sum = 0;
+            dbg!(line);
+        } else {
+            sum += line.parse::<usize>().unwrap();
         }
     }
+    print!("elves array = {:?} max elves calories = {}",elves, elves.iter().max().unwrap());
 }
