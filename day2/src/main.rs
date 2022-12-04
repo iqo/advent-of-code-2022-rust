105308814180d7f8f1e5dee85a50bfe00e150eb5
iqo <niklas.brynolf@gmail.com>
2022-12-04 21:51:58 +0100

    start of day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::cmp::Ordering;
+const TESTDATA: &str = "
+A Y
+B X
+C Z
+";
+// #[derive(Eq)]
+enum PlayerMove {
+    Rock,
+    Paper,
+    Scissor,
 }
+enum MatchOutcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+impl PlayerMove {
+    fn player_move(&self) -> i32 {
+        match self {
+            PlayerMove::Rock => 1,
+            PlayerMove::Paper => 2,
+            PlayerMove::Scissor => 3
+        }
+    }
+}
+fn main() {
+    /*     let file_input: String = read_input_file();
+     */
+    println!("{}", TESTDATA);
+}
+/* fn read_input_file() -> String {
+    std::fs::read_to_string("src/input.txt").unwrap()
+} */
