ff10e285ac8a953cabe5ba940a5b1233385098a9
iqo <niklas.brynolf@gmail.com>
2022-12-08 21:36:08 +0100

    strat part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,7 +6,7 @@ PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
-fn priority(b: u8) -> i32 {
+fn backpack_priority_table(b: u8) -> i32 {
     (97..=122) // assci a till z
         .chain(65..=90) //assci A till Z
         .zip(1..=52)
@@ -16,14 +16,16 @@ fn part_one(input: &str) -> i32{
 input.lines().map(|f| {
     let (a, b) = f.split_at(f.len() / 2);
     let backpack_index = a.find(&b.chars().collect::<Vec<_>>()[..]).unwrap();
-    priority(a.as_bytes()[backpack_index]) // get value index on split string
+    backpack_priority_table(a.as_bytes()[backpack_index]) // get value index on split string
 }).sum()
 }
 fn main() { 
 let file_input: String = read_input_file();
 let part_one_backpacks_test_data = part_one(TESTDATA);
 let part_one_backpacks = part_one(&file_input);
+let part_two_backpacks_test_data = part_one(TESTDATA);
     assert_eq!(part_one_backpacks_test_data, 157);
+    assert_eq!(part_two_backpacks_test_data, 70);
     println!("{}", part_one_backpacks);
 }
 fn read_input_file() -> String {
