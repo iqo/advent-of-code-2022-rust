7c1ac1a64301f12af77a20f9e01b8859b6ebad7f
iqo <niklas.brynolf@gmail.com>
2022-12-08 22:09:10 +0100

    small fixes confusion on part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::{HashMap, HashSet}};
 
 const TESTDATA: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -13,17 +13,21 @@ fn backpack_priority_table(b: u8) -> i32 {
         .collect::<HashMap<_, _>>()[&b]
 }
 fn part_one(input: &str) -> i32{
-input.lines().map(|f| {
-    let (a, b) = f.split_at(f.len() / 2);
+input.lines().map(|line| {
+    let (a, b) = line.split_at(line.len() / 2);
     let backpack_index = a.find(&b.chars().collect::<Vec<_>>()[..]).unwrap();
     backpack_priority_table(a.as_bytes()[backpack_index]) // get value index on split string
 }).sum()
+}
+fn part_two(input: &str) -> i32 {
+/*     input.lines().map(|line| HashSet::from_iter(line.bytes())).collect::<Vec<_>>().chunks(3)
+ */    3
 }
 fn main() { 
 let file_input: String = read_input_file();
 let part_one_backpacks_test_data = part_one(TESTDATA);
 let part_one_backpacks = part_one(&file_input);
-let part_two_backpacks_test_data = part_one(TESTDATA);
+let part_two_backpacks_test_data = part_two(TESTDATA);
     assert_eq!(part_one_backpacks_test_data, 157);
     assert_eq!(part_two_backpacks_test_data, 70);
     println!("{}", part_one_backpacks);
