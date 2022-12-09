77b02e238b08b2b05a992bd209dfad284a16261b
iqo <niklas.brynolf@gmail.com>
2022-12-09 23:42:38 +0100

    day 4

---
+++ day4/src/main.rs
@@ -0,0 +1,14 @@
+const TESTDATA: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+fn part_one() -> i32 { 
+    2
+}
+fn main() {
+    let part_one_test_data = part_one();
+    assert_eq!(part_one_test_data, 2);
+}
